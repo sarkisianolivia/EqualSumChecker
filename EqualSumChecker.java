1bf8fe27ed9600e4149b4edd8f9da0edfabef613
Olivia Sarkisian <115608329+sarkisianolivia@users.noreply.github.com>
2022-10-31 22:33:35 -0500

    Add files via upload

---
+++ EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum (int myFirstInt, int mySecondInt, int myThirdInt) {
+
+        if (myFirstInt + mySecondInt == myThirdInt) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    
+}
